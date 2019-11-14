c55428fe548b01896f37211bfcd9db7b44cfc9a7
Dilyar Arkin <daerken118@gmail.com>
2019-11-13 19:50:17 -0500

    first virson

---
+++ src/test/java/Gmail/Credentials.java
@@ -0,0 +1,16 @@
+package Gmail;
+
+public class Credentials {
+
+    public class Credential {
+        private String userName = "";
+        private String passWord = "";
+        public String getUserName(){
+            return userName;
+        }
+        public String getPassWord(){
+            return passWord;
+        }
+    }
+
+}
